2467e6af612f59cbcb4e348321fc01c7df43bff5
IdanKok <wildan.aruman90@gmail.com>
2023-03-21 14:27:46 +0700

    Finish - Integrate Edit Profile Featured

--- pages/my-profile.tsx
+++ pages/my-profile.tsx
@@ -6,6 +6,12 @@ import { useEffect, useRef } from "react";
 import TextInput from "../components/TextInput";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { useRouter } from "next/router";
+import useUserQuery from "../hooks/queries/use-user-query";
+import useEditProfileMutation from "../hooks/mutations/use-edit-profile-mutation";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import Loading from "react-spinners/BeatLoader";
 
 type FormValues = {
   fullname: string;
@@ -20,7 +26,11 @@ const MyProfileSchema = Yup.object().shape({
 });
 
 const MyProfilePage: NextPage = () => {
+  const router = useRouter();
+  const editProfileMutation = useEditProfileMutation();
   const refContentInput = useRef<HTMLTextAreaElement>(null);
+  const userQuery = useUserQuery();
+  const queryClient = useQueryClient();
 
   const initialValues: FormValues = {
     fullname: "",
@@ -31,22 +41,30 @@ const MyProfilePage: NextPage = () => {
     initialValues,
     validationSchema: MyProfileSchema,
     validateOnMount: true,
-    onSubmit: () => {
-      alert("Saved !");
+    onSubmit: async (values) => {
+      try {
+        const response = await editProfileMutation.mutateAsync({
+          name: values.fullname,
+          picture: values.photo,
+        });
+
+        queryClient.setQueryData(["user"], response);
+        router.push("/");
+        toast.success("Edit Profile Succesfull !");
+      } catch (error) {
+        toast.error("Failed to edit profile");
+      }
     },
   });
 
-  const user = {
-    fullname: "John Doe",
-    photo: "/images/dummy-avatar.png",
-  };
+
 
   useEffect(() => {
     formik.setValues({
-      fullname: user.fullname,
+      fullname: userQuery.data?.name || "",
       photo: null,
     });
-  }, []);
+  }, [userQuery.data]);
 
   return (
     <div>
@@ -61,24 +79,30 @@ const MyProfilePage: NextPage = () => {
         submitLabel="Save"
         onClickSubmit={formik.handleSubmit}
       />
-      <div className="w-[400px] flex flex-col justify-center items-center mx-auto py-24">
-      <PhotoPicker
-          preview={user.photo}
-          onPick={(file) => formik.setFieldValue("photo", file)}
-        />
-        <div className="h-16"/>
-        <TextInput
-          name="fullname"
-          label="Full Name"
-          placeholder="Enter your full name"
-          type="text"
-          value={formik.values.fullname}
-          hasError={!!formik.errors.fullname}
-          errorMessage={formik.errors.fullname}
-          onChange={formik.handleChange}
-        />
-     
-      </div>
+      {editProfileMutation.isLoading && (
+        <div className=" h-screen flex justify-center items-center">
+          <Loading size={16} color="rgb(30 64 175)" />
+        </div>
+      )}
+      {!editProfileMutation.isLoading && (
+        <div className="w-[400px] flex flex-col justify-center items-center mx-auto py-24">
+          <PhotoPicker
+            preview={userQuery.data?.picture}
+            onPick={(file) => formik.setFieldValue("photo", file)}
+          />
+          <div className="h-16" />
+          <TextInput
+            name="fullname"
+            label="Full Name"
+            placeholder="Enter your full name"
+            type="text"
+            value={formik.values.fullname}
+            hasError={!!formik.errors.fullname}
+            errorMessage={formik.errors.fullname}
+            onChange={formik.handleChange}
+          />
+        </div>
+      )}
     </div>
   );
 };
