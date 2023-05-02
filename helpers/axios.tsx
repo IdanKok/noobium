b91851884d54542a3f0f198b00566ff6a78ec33b
IdanKok <wildan.aruman90@gmail.com>
2023-05-02 17:23:49 +0700

    refinish

--- helpers/axios.tsx
+++ helpers/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://167.172.70.208:8003/api',
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 
     headers: {
         "Content-Type": "application/json",
